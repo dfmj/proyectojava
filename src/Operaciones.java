cc4ac9f6ef362271cd60170f3ac3269da785eb8f
RA302 <daniel.malaver01@uptc.edu.co>
2022-06-29 09:57:05 -0500

    mi primer commit

---
+++ src/Operaciones.java
@@ -0,0 +1,19 @@
+
+public class Operaciones {
+
+	public static void main(String[] args) {
+		char c='ñ';
+		String s="A";
+		System.out.println(c+s);
+		
+		
+				
+	    
+	    		
+        
+			
+		
+
+	}
+
+}
